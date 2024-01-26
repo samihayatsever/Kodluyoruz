39a50756fe2266ef6beb9114ad01ee391f2fce5d
sami <samihayatsever@gmail.com>
2024-01-26 18:33:26 +0300

    first springboot project

---
+++ src/main/java/com/samih/kodluyoruzspringboot/KodluyoruzspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.samih.kodluyoruzspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodluyoruzspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodluyoruzspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samih/kodluyoruzspringboot/KodluyoruzspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samih.kodluyoruzspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
