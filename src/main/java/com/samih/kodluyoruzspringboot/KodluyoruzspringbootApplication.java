76c286533cc328207096659650da5f19583a10b8
sami <samihayatsever@gmail.com>
2024-01-27 17:22:42 +0300

    security exclude

--- src/main/java/com/samih/kodluyoruzspringboot/KodluyoruzspringbootApplication.java
+++ src/main/java/com/samih/kodluyoruzspringboot/KodluyoruzspringbootApplication.java
@@ -3,11 +3,13 @@ package com.samih.kodluyoruzspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+	org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class KodluyoruzspringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KodluyoruzspringbootApplication.class, args);
 	}
-
 }
