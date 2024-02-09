4b2d8a00a94ee7371dadab178c440afad2739576
sami <samihayatsever@gmail.com>
2024-02-09 21:48:27 +0300

    BaseEntity and EmployeeEntity

---
+++ src/main/java/com/samih/entity/BaseEntity.java
@@ -0,0 +1,55 @@
+package com.samih.entity;
+
+import java.sql.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @Column(name="id",nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private Date createdDate;
+
+    @Column(name = "update_by")
+    @LastModifiedBy
+    private String updateBy;
+
+    @Column(name = "update_date")
+    @LastModifiedDate
+    private Date updateDate;
+
+    @Column(name = "system_auto_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Date date;
+}
---
+++ src/main/java/com/samih/entity/EmployeeEntity.java
@@ -0,0 +1,38 @@
+package com.samih.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+
+@Data
+@NoArgsConstructor
+@Builder
+@Log4j2
+
+@Entity
+@Table(name = "employees")
+public class EmployeeEntity extends BaseEntity implements Serializable {
+    
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_name")
+    private Long emailId;
+
+    public EmployeeEntity(String firstName, String lastName, Long emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+    
+}
