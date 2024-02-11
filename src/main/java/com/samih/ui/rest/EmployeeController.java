d30dcdd8559dcf84c9c173bdcdbb93ff05c95502
sami <samihayatsever@gmail.com>
2024-02-11 14:43:57 +0300

    employeeController (@RestController)

---
+++ src/main/java/com/samih/ui/rest/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.samih.ui.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+    
+}
